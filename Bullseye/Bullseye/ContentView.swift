2eb299955fb4f5e7fe0889e1f1592f25a55317b9
Ana Carolina Martins Pessoa <anamartinspessoa@icloud.com>
2022-04-16 18:42:24 -0300

    Doing the view

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -10,13 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-                Text("🎯🎯🎯 \n Put the BullSEye as close as you can to")
-                    .padding()
+            Text("🎯🎯🎯 \n Put the BullSEye as close as you can to")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
             Text("89")
+                .font(.largeTitle)
+                .kerning(-1.0)
+                .fontWeight(.black)
             HStack {
                 Text("1")
+                    .font(.body)
+                    .fontWeight(.bold)
+                   
                 Slider(value: .constant(70), in: 1.0...100.0)
                 Text("100")
+                    .font(.body)
+                    .fontWeight(.bold)
             }
             Button(action: {}) {
                 Text("Hit me")
@@ -28,6 +40,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
         ContentView()
             .previewLayout(.fixed(width: 568, height: 320))
     }
